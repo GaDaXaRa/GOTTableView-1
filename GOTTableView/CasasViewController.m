41755acdfc45fa0a79fa2a3665ef50954a99ff26
Ricardo <odrakir@gmail.com>
2014-06-10 20:36:39 +0200

    Practica Extra - Loop

--- GOTTableView/CasasViewController.m
+++ GOTTableView/CasasViewController.m
@@ -15,7 +15,9 @@
 @property (nonatomic, strong) GotModel* modelo;
 @end
 
-@implementation CasasViewController
+@implementation CasasViewController {
+    BOOL _usuarioHizoScroll;
+}
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
@@ -32,6 +34,10 @@
     
     self.modelo = [[GotModel alloc] init];
     [self.modelo cargaModelo];
+    
+    self.tableView.showsVerticalScrollIndicator = NO;
+    
+ //   self.edgesForExtendedLayout = UIRectEdgeNone;
     
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
@@ -50,14 +56,15 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return self.modelo.casas.count;
+    return self.modelo.casas.count*2;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     CasaCell *cell = [tableView dequeueReusableCellWithIdentifier:@"casaCell" forIndexPath:indexPath];
     
-    Casa* casa = [self.modelo.casas objectAtIndex:indexPath.row];
+    Casa* casa = [self.modelo.casas objectAtIndex:indexPath.row%self.modelo.casas.count];
+    
     cell.nombre.text = casa.nombre;
     cell.lema.text = casa.lema;
     cell.imagen.image = [UIImage imageNamed:casa.imagen];
@@ -77,53 +84,27 @@
     [self.delegate casaSeleccionada:casa.nombre];
     [self.navigationController popViewControllerAnimated:YES];
 }
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
 {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+    NSLog(@"%f", scrollView.contentOffset.y);
+    _usuarioHizoScroll = YES;
 }
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
+
+    if(scrollView.contentSize.height<=0.0||!_usuarioHizoScroll)
+        return;
+    
+    if((scrollView.contentOffset.y+scrollView.frame.size.height)>=scrollView.contentSize.height) {
+        NSLog(@"%f + %f >= %f", scrollView.contentOffset.y, scrollView.frame.size.height, scrollView.contentSize.height);
+        scrollView.contentOffset = CGPointMake(0, scrollView.contentSize.height/2-scrollView.frame.size.height);
+    }
+    
+    if(scrollView.contentOffset.y<=-64) {
+        NSLog(@"%f <= 0", scrollView.contentOffset.y);
+        
+        scrollView.contentOffset = CGPointMake(0, scrollView.contentSize.height/2-64);
+    }
 }
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 @end
