8f50af8381883724ff5d303502e3bbbd6fd03376
Ricardo <odrakir@gmail.com>
2014-06-10 18:01:22 +0200

    Practica 9 - Agregar celdas MAL

--- GOTTableView/TablaViewController.m
+++ GOTTableView/TablaViewController.m
@@ -58,6 +58,32 @@
     }
 }
 
+- (void)setEditing:(BOOL)editing animated:(BOOL)animated
+{
+    [super setEditing:editing animated:animated];
+    
+    [self.tableView beginUpdates];
+
+    if(editing) {
+        
+        for (int c = 0; c<self.modelo.casas.count; c++) {
+            Casa* casa = [self.modelo.casas objectAtIndex:c];
+            NSIndexPath* indexPath = [NSIndexPath indexPathForRow:casa.personajes.count inSection:c];
+            [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+        }
+    } else {
+        
+        for (int c = 0; c<self.modelo.casas.count; c++) {
+            Casa* casa = [self.modelo.casas objectAtIndex:c];
+            NSIndexPath* indexPath = [NSIndexPath indexPathForRow:casa.personajes.count inSection:c];
+            [self.tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+        }
+    }
+    
+    [self.tableView endUpdates];
+
+}
+
 #pragma mark - DetailViewController Delegate
 - (void)mataPersonaje:(Personaje *)personaje
 {
@@ -93,15 +119,24 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     Casa* casa = [self.modelo.casas objectAtIndex:section];
+    
+    if(self.editing)
+        return casa.personajes.count + 1;
+    
     return casa.personajes.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    
-    PersonajeCell *cell = [tableView dequeueReusableCellWithIdentifier:@"celdaPersonaje" forIndexPath:indexPath];
-    
     Casa* casa = [self.modelo.casas objectAtIndex:indexPath.section];
+    if(indexPath.row>=casa.personajes.count && self.isEditing) {
+        UITableViewCell* celda = [tableView dequeueReusableCellWithIdentifier:@"celdaAdd" forIndexPath:indexPath];
+        celda.textLabel.text = @"Añadir Personaje";
+        return celda;
+    }
+
+    PersonajeCell *cell = [tableView dequeueReusableCellWithIdentifier:@"celdaPersonaje" forIndexPath:indexPath];
+
     Personaje* personaje = [casa.personajes objectAtIndex:indexPath.row];
     
     cell.nombre.text = personaje.nombre;
@@ -163,17 +198,58 @@
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    if(editingStyle==UITableViewCellEditingStyleDelete) {
-        Casa* casa = [self.modelo.casas objectAtIndex:indexPath.section];
-        NSMutableArray* auxPersonajes = casa.personajes.mutableCopy;
-        [auxPersonajes removeObjectAtIndex:indexPath.row];
-        casa.personajes = auxPersonajes.copy;
-        
-        [self.tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+    switch (editingStyle) {
+        case UITableViewCellEditingStyleDelete: {
+            Casa* casa = [self.modelo.casas objectAtIndex:indexPath.section];
+            NSMutableArray* auxPersonajes = casa.personajes.mutableCopy;
+            [auxPersonajes removeObjectAtIndex:indexPath.row];
+            casa.personajes = auxPersonajes.copy;
+            
+            [self.tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+        }
+            break;
+            
+        case UITableViewCellEditingStyleInsert: {
+            Casa* casa = [self.modelo.casas objectAtIndex:indexPath.section];
+            Personaje* personaje = [[Personaje alloc] init];
+            personaje.nombre = @"Nuevo personaje";
+            [casa addPersonaje:personaje];
+            
+            [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+        }
+            break;
+        default:
+            break;
     }
+    
 }
 
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    Casa* casa = [self.modelo.casas objectAtIndex:indexPath.section];
+    if(indexPath.row >= casa.personajes.count)
+        return UITableViewCellEditingStyleInsert;
+    else
+        return UITableViewCellEditingStyleDelete;
+}
 
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    Casa* casa = [self.modelo.casas objectAtIndex:indexPath.section];
+    if(indexPath.row >= casa.personajes.count)
+        return NO;
+    else
+        return YES;
+    
+}
 
+- (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    Casa* casa = [self.modelo.casas objectAtIndex:indexPath.section];
+    if(self.isEditing && indexPath.row<casa.personajes.count)
+        return nil;
+    
+    return indexPath;
+}
 
 @end
